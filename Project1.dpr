604759889dee5f6e9c3334a11cff92cab1167f52
Guilherme Tavares Pinheiro <gui.tavares2002.contato@gmail.com>
2023-03-05 15:23:32 -0300

    add form

---
+++ Project1.dpr
@@ -0,0 +1,18 @@
+program Project1;
+
+
+
+{$R *.dres}
+
+uses
+  Vcl.Forms,
+  unitPrincipal in 'unitPrincipal.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
